89201b2f5780e0a521f9e3d92382f7eddd44f095
yaoxuchen <yaoxuchen@gmail.com>
2013-09-14 10:58:56 +0000

    more path name change

--- jacana-xy/scripts-align/align2Files.sh
+++ jacana-xy/scripts-align/align2Files.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-java -DJACANA_HOME=../ -jar ../build/lib/jacana-align.jar -m fr-en.model -src fr -tgt en -a s1.txt -b s2.txt -o s.align
+java -DJACANA_HOME=../ -jar ../build/lib/jacana-xy.jar -m fr-en.model -src fr -tgt en -a s1.txt -b s2.txt -o s.align
--- jacana-xy/scripts-align/runDemoServer.sh
+++ jacana-xy/scripts-align/runDemoServer.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-java -DJACANA_HOME=../ -cp ../build/lib/jacana-align.jar Demo fr-en.model 8080
+java -DJACANA_HOME=../ -cp ../build/lib/jacana-xy.jar Demo fr-en.model 8080
 
